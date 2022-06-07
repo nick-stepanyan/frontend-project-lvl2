8cd28a73e25e4cd8d6d42325b34108bd730cebc2
nick-stepanyan <nick.win.32@mail.ru>
2022-06-07 18:56:16 +0300

    My first step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,16 +5,16 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('CLI to some JavaScript string utilities')
-    .version('0.8.0');
+  .name('gendiff')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
 
 program.command('split')
-    .description('Split a string into substrings and display as an array')
+  .description('Split a string into substrings and display as an array')
 
-.action((str, options) => {
+  .action((str, options) => {
     const limit = options.first ? 1 : undefined;
     console.log(str.split(options.separator, limit));
-});
+  });
 
 program.parse();
