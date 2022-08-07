2db641e0d069de9d487b45974dcfeb1eadd40fb0
nick-stepanyan <nick.win.32@mail.ru>
2022-08-07 13:09:46 +0300

    gendiff correted

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,15 +6,15 @@ import genDiff from '../src/index.js';
 
 const program = new Command();
 program
-  .name('genDiff')
-  .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
-  .version('0.1.0', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format', 'stylish')
-  .arguments('<filepath1>', '<filepath2>')
-  .action((filepath1, filepath2, options) => {
-    // eslint-disable-next-line no-console
-    console.log(genDiff(filepath1, filepath2, options.format));
-  });
+    .name('genDiff')
+    .description('Compares two configuration files and shows a difference.')
+    .helpOption('-h, --help', 'output usage information')
+    .version('0.1.0', '-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2, options) => {
+        // eslint-disable-next-line no-console
+        console.log(genDiff(filepath1, filepath2, options.format));
+    });
 
 program.parse();
