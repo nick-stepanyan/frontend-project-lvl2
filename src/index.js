6e1f4ee244e18e8615a84e9d5b27a283cb949efa
nick-stepanyan <nick.win.32@mail.ru>
2022-08-11 13:17:40 +0300

    change some revie

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import format from './formatters/index.js';
 
 const readFileData = (filepath) => readFileSync(path.resolve(process.cwd(), filepath));
 const getExtension = (filepath) => path.extname(filepath);
-const getParserData = (filepath) => {
+const getData = (filepath) => {
   const expansion = getExtension(filepath).slice(1);
   return parseData(readFileData(filepath), expansion);
 };
 
 const genDiff = (filepath1, filepath2, option = 'stylish') => {
-  const diff = buildDiff(getParserData(filepath1), getParserData(filepath2));
+  const diff = buildDiff(getData(filepath1), getData(filepath2));
   return format(diff, option);
 };
 export default genDiff;
