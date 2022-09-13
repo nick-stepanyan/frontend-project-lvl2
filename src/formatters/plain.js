f7dd6e3b1296cf5cc35cd19c793baf9adc5e0196
nick-stepanyan <nick.win.32@mail.ru>
2022-09-13 14:52:31 +0300

    sss

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,34 +1,35 @@
 import _ from 'lodash';
 
 const stringify = (data) => {
-  if ((_.isObject(data))) {
-    return '[complex value]';
-  }
-  return typeof data === 'string' ? `'${String(data)}'` : String(data);
+    if ((_.isObject(data))) {
+        return '[complex value]';
+    }
+    return typeof data === 'string' ? `'${String(data)}'` : String(data);
 };
 
 const getPath = (path, name) => [...path, name].join('.');
 
 const plain = (diff) => {
-  const iter = (currentValue, path) => {
-    const result = currentValue.map((data) => {
-      switch (data.type) {
-        case 'added':
-          return `Property '${getPath(path, data.name)}' was added with value: ${stringify(data.value)}`;
-        case 'deleted':
-          return `Property '${getPath(path, data.name)}' was removed`;
-        case 'changed':
-          return `Property '${getPath(path, data.name)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
-        case 'nested':
-          return iter(data.value, [...path, data.name]);
-        case 'unchanged':
-          return null;
-        default:
-          throw new Error(`Unknown state: '${data.type}'!`);
-      }
-    });
-    return result.join('/n');
-  };
-  return `${iter(diff, [])}`;
+    const iter = (currentValue, path) => {
+        const result = currentValue.map((data) => {
+            switch (data.type) {
+                case 'added':
+                    return `Property '${getPath(path, data.name)}' was added with value: ${stringify(data.value)}`;
+                case 'deleted':
+                    return `Property '${getPath(path, data.name)}' was removed`;
+                case 'changed':
+                    return `Property '${getPath(path, data.name)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
+                case 'nested':
+                    return iter(data.value, [...path, data.name]);
+                case 'unchanged':
+                    return null;
+                default:
+                    throw new Error(`Unknown state: '${data.type}'!`);
+            }
+        });
+
+        _.compact(result).join('\n');
+    };
+    return `${iter(diff, [])}`;
 };
 export default plain;
