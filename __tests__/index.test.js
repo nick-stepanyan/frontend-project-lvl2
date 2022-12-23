dc32dd5f8a5a789f599af0e439417a10e0270446
nick-stepanyan <nick.win.32@mail.ru>
2022-12-23 12:24:07 +0300

    fix bug testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,23 +5,23 @@ import { dirname, join } from 'path';
 import genDiff from '../src/index';
 
 const __filename = fileURLToPath(
-  import.meta.url,
+    import.meta.url,
 );
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (file) => readFileSync(getFixturePath(file), 'utf8');
+const readFixture = (file) => readFileSync(getFixturePath(file), 'utf8').trim();
 
 const stylishResult = readFixture('expected.txt');
 const plainResult = readFixture('expectedPlain.txt');
 const jsonResult = readFixture('expectedJson.txt');
 
 describe('Gendiff_test', () => {
-  test.each(['json', 'yml'])('calculate differences', (extenshion) => {
-    const filepath1 = getFixturePath(`file1.${extenshion}`);
-    const filepath2 = getFixturePath(`file2.${extenshion}`);
-    expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
-    expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
-  });
+    test.each(['json', 'yml'])('calculate differences', (extenshion) => {
+        const filepath1 = getFixturePath(`file1.${extenshion}`);
+        const filepath2 = getFixturePath(`file2.${extenshion}`);
+        expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
+        expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
+        expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
+        expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
+    });
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,15 +2,15 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 const formatOutput = (diff, type) => {
-  switch (type) {
-    case 'plain':
-      return plain(diff);
-    case 'json':
-      return JSON.stringify(diff, null, 2);
-    case 'stylish':
-      return stylish(diff);
-    default:
-      throw new Error(`${type} - invalid type of format!`);
-  }
+    switch (type) {
+        case 'plain':
+            return plain(diff);
+        case 'json':
+            return JSON.stringify(diff);
+        case 'stylish':
+            return stylish(diff);
+        default:
+            throw new Error(`${type} - invalid type of format!`);
+    }
 };
 export default formatOutput;
