23b202c45b2a193ec83240b30e3ce2863d3c3981
nick-stepanyan <nick.win.32@mail.ru>
2022-12-23 12:27:06 +0300

    fix lint rules

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ import { dirname, join } from 'path';
 import genDiff from '../src/index';
 
 const __filename = fileURLToPath(
-    import.meta.url,
+  import.meta.url,
 );
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
@@ -16,12 +16,12 @@ const plainResult = readFixture('expectedPlain.txt');
 const jsonResult = readFixture('expectedJson.txt');
 
 describe('Gendiff_test', () => {
-    test.each(['json', 'yml'])('calculate differences', (extenshion) => {
-        const filepath1 = getFixturePath(`file1.${extenshion}`);
-        const filepath2 = getFixturePath(`file2.${extenshion}`);
-        expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
-        expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
-        expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
-        expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
-    });
+  test.each(['json', 'yml'])('calculate differences', (extenshion) => {
+    const filepath1 = getFixturePath(`file1.${extenshion}`);
+    const filepath2 = getFixturePath(`file2.${extenshion}`);
+    expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
+  });
 });
